06113ee9e61f2baa59415b734ca4528f76e12dc2
avangan <38505183+avangan@users.noreply.github.com>
2019-01-17 15:41:41 -0600

    Add files via upload

---
+++ labC/labC/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace lab_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlds Second time !");
+        }
+    }
+}
---
+++ labC/labC/Quiz.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring and array that holds the correct answers. You name the array and add it's datatype. datatype[] arrayName;
+            string[] corAns;
+
+            // Initializing the Array to assign how many indexes it will have there are 10 questions we will have 10 correct answers
+            string[] corAns = { "A", "A", "B" };
+
+
+
+            // declaring an array for end users selection
+
+
+            // Initializing the Array for 10 Users answers
+            string[] UseAns = new string[10];
+
+            int start; ///public variable declaration
+
+
+
+            Console.WriteLine("Welcome to the NetCore Quiz");
+            Console.WriteLine("Test line for git hub only");
+            Console.WriteLine("Let's get started, please enter any number to enter the program");
+            start = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
